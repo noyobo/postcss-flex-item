0f787674a5f9136ff4587e16f5aac1f3b3dd91f2
noyobo <noyobo@gmail.com>
2020-10-10 17:06:39 +0800

    ci: init

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  useTabs: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
